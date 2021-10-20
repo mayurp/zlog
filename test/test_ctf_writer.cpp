f77e13485476638c968d487f2d5e16266e775ad1
Mayur Patel <mayurp@gmail.com>
2021-10-20 04:16:25 +0100

    Add more no copy tests

--- test/test_ctf_writer.cpp
+++ test/test_ctf_writer.cpp
@@ -106,19 +106,31 @@ TEST_CASE("barectf::payload_size dynamic containers")
 // Compile time checks
 struct NonCopyable
 {
-    NonCopyable(int _a) : a(_a) {}
-    NonCopyable(const NonCopyable& x) = delete; // : a(x.a) { std::cout << "copy construct\n";}
-    NonCopyable(NonCopyable&& x) = delete; // : a(x.a) { std::cout << "move construct\n";}
-    NonCopyable& operator=(const NonCopyable& x)  = delete; //  { a = x.a; std::cout << "copy assign\n"; return *this;}
-    NonCopyable& operator=(NonCopyable&& x )  = delete; //  { a = x.a; std::cout << "move assign\n"; return *this;}
+    NonCopyable(int _i) : i(_i) {}
+    NonCopyable(const NonCopyable& x) = delete;
+    NonCopyable(NonCopyable&& x) = delete;
+    NonCopyable& operator=(const NonCopyable& x);
+    NonCopyable& operator=(NonCopyable&& x );
     
-    int a = 0;
+    int i = 0;
 };
-REFLECT(NonCopyable, a)
+REFLECT(NonCopyable, i)
+
+struct Aggregate
+{
+    Aggregate(int i) : n(i) {}
+    NonCopyable n;
+};
+REFLECT(Aggregate, n)
+
 
 TEST_CASE("logging nocopy")
 {
     NonCopyable n(1);
     LOGD("test no copy lvalue: {value}", n);
     LOGD("test no copy rvalue: {value}", NonCopyable(1));
+    
+    Aggregate a(2);
+    LOGD("test aggregate no copy lvalue: {value}", a);
+    LOGD("test aggregate no copy rvalue: {value}", Aggregate(2));
 }
